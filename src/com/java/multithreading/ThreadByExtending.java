2a11bf26e6e393d2f3ee4b769168e0376739bb33
singhbikash123 <singhbikash123@gmail.com>
2023-11-23 22:19:36 -0600

    Thread impl correction'

--- src/com/java/multithreading/ThreadByExtending.java
+++ src/com/java/multithreading/ThreadByExtending.java
@@ -1,42 +1,34 @@
 package com.java.multithreading;
+
 /**
  * @author Bikash Singh
  */
 public class ThreadByExtending extends Thread {
-	private String threadName;
-	
-	public String getThreadName() {
-		return threadName;
-	}
-	public void setThreadName(String threadName) {
-		this.threadName = threadName;
-	}
-	public ThreadByExtending() {}
+	private final String name;
+
 	public ThreadByExtending(String name) {
 		super(name);
-		this.threadName = name;
-	
-		
+		this.name = name;
+
 	}
-	
+
 	@Override
 	public void run() {
-		for(int i = 0;i<5;i++) {
-			//System.out.println(Thread.currentThread().getName());
+		for (int i = 0; i < 5; i++) {
+			// System.out.println(Thread.currentThread().getName());
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			System.out.println("Thread current thread::"+Thread.currentThread().getName()+ "Value of i::"+i);
+			System.out.println("Thread current thread::" + Thread.currentThread().getName() + "Value of i::" + i);
 		}
 	}
-	
-	
+
 	public static void main(String args[]) {
-		System.out.println("Thread name:::"+Thread.currentThread().getName());
-		ThreadByExtending t1 = new ThreadByExtending("Thread 1 ");
-		ThreadByExtending t2 = new ThreadByExtending("Thread 2 ");
+		System.out.println("Thread name:::" + Thread.currentThread().getName());
+		Thread t1 = new ThreadByExtending("Thread 1 ");
+		Thread t2 = new ThreadByExtending("Thread 2 ");
 		System.out.println(Thread.currentThread().getName());
 		t1.start();
 		t2.start();
@@ -47,10 +39,7 @@ public class ThreadByExtending extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
-	
 
 }
